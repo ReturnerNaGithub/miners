75b2283debcd4e35a1ce6f46e7926257f1bb2d8f
hylcore-V <hyliver0109@gmail.com>
2024-12-03 08:58:11 -0500

    style: responsive design

--- components/Footer2.tsx
+++ components/Footer2.tsx
@@ -5,58 +5,50 @@ const Footer2 = () => {
 
 
     <div>
-    <div style={{
-    width:"100%", 
-    height:"260px",
-    display:"flex",
-    gap:"60px",
-    padding:"20px",
-    marginTop:"22px",
-    alignItems: 'center',
-justifyContent: "center",
-    flexDirection:"row"
-      
-      
-    }}>
-      
-
-      <div className="mt-0">
+      <div className='news'>
         <img
           src="https://white-reasonable-barnacle-530.mypinata.cloud/ipfs/QmNrfUAJ1E3WqtnwZL9XnTW37ZZVsz3AJcuCPVuzHMATnr/free%202play...png"
           alt="reklama3"
           width={480}
           height={300}
+          style={{
+            width: "100%",
+            height: "auto",
+            objectFit: "contain",
+          }}
         />
-      </div>
-      <div className="mb-0">
         <img
           src="https://white-reasonable-barnacle-530.mypinata.cloud/ipfs/QmNrfUAJ1E3WqtnwZL9XnTW37ZZVsz3AJcuCPVuzHMATnr/hero%20reklama.png"
           alt="reklama4"
           width={480}
           height={300}
+          style={{
+            width: "100%",
+            height: "auto",
+            objectFit: "contain",
+          }}
         />
+
+
       </div>
-     
 
+      <div>
+        <h2
+          style={{
+            color: "white",
+            fontSize: "1.2rem",
+            fontWeight: "bold",
+            marginTop: "0.5rem",
+            marginBottom:"0.5rem",
+            textAlign: "center",
+            justifyContent: "center",
+          }}
+        >
+          Powered by Gaming Studio Damana
+        </h2>
+      </div>
     </div>
 
-    <div>
-    <h2 
-  style={{
-    color: "white", 
-    fontSize: "1.2rem", 
-    fontWeight: "bold", 
-    marginTop: "0.5rem",
-    marginBottom:"0.5rem", 
-    textAlign: "center",
-    justifyContent: "center",
-  }}
->
-  Powered by Gaming Studio Damana
-</h2>
-    </div>
-    </div>
-  
   )
 }
 
--- components/Head.tsx
+++ components/Head.tsx
@@ -7,36 +7,29 @@ const Head = () => {
   return (
 
 
-            <div style= {{
+    <div className='header'>
+      <img
+        src="https://white-reasonable-barnacle-530.mypinata.cloud/ipfs/Qma49LjDEwqz3TePcBrWUwtviui9YJwHEHqqXXShYy4jgp/Miners%20nation%20-%20LOGO%20-%20BG.png"
+        alt="logo"
+        height={100}
+        width={200}
+        className='logo'
+      />
 
-flexDirection: 'row',
-width: "100%",
-height:"60px",
-display: "flex", 
-gap: "1140px",
-alignItems: 'flex-start', // Align items to the left
-    justifyContent: 'flex-start', // Align items to the top
-    padding: "10px 4px",
-
-            }}>
-                <img
-  src="https://white-reasonable-barnacle-530.mypinata.cloud/ipfs/Qma49LjDEwqz3TePcBrWUwtviui9YJwHEHqqXXShYy4jgp/Miners%20nation%20-%20LOGO%20-%20BG.png"
-  alt="logo"
-  height={100}
-  width={200}
-  style={{ marginTop: -20 }}
-/>
- 
-<ConnectButton
-client = {client}
-chain= {chain}
+      <ConnectButton
+        client = {client}
+        chain= {chain}
+        connectButton={{
+          style: {
+          minWidth: "165px",
+          height: "50px",
+          },
+          className: 'connect-btn', // Custom CSS class for additional styling
+        }}
+      />
+    </div>
 
 
-
-/> 
-            </div>
-
-    
   )
 }
 
